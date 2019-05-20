2d97fda169445f1ebd374ee2fcfd4b1a49ada4cc
isiaka.ismaila@gmail.com <ishmael@Ishmaels-MacBook-Pro.local>
2019-05-20 06:43:15 +0100

    Added my Codepen account to footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faLinkedinIn, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { 
+    faTwitter, 
+    faLinkedinIn, 
+    faGithub, 
+    faCodepen,
+    faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 export function Footer() {
     return (
@@ -16,18 +21,23 @@ export function Footer() {
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.linkedin.com/in/isiaka-ismaila-1b109baa/" id="linkedin">
+                        <a href="https://www.linkedin.com/in/isiaka-ismaila-1b109baa/" target="_blank" id="linkedin">
                             <FontAwesomeIcon icon={ faLinkedinIn } />
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.instagram.com/ishmael.codes/" id="instagram">
+                        <a href="https://www.instagram.com/ishmael.codes/" target="_blank" id="instagram">
                             <FontAwesomeIcon icon={ faInstagram } />
                         </a>
                     </li>
                     <li>
-                        <a href="https://github.com/ishmaell" id="github">
+                        <a href="https://github.com/ishmaell" target="_blank" id="github">
                             <FontAwesomeIcon icon={ faGithub } />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://codepen.io/ish_maell" target="_blank" id="codepen">
+                            <FontAwesomeIcon icon={ faCodepen } />
                         </a>
                     </li>
                 </ul>
